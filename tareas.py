78d5ffac654b8e04991d062f81ae81618bada9ef
Mrpancetita <contry_1990@hotmail.com>
2025-01-26 21:14:23 +0100

    wip

--- tareas.py
+++ tareas.py
@@ -1,10 +1,13 @@
-# Created by modifying the original work of https://github.com/josepg
+# Created by modifying the original idea of https://github.com/josepg
 
 import config, re, ssl, sys
 import requests
 from termcolor import cprint
 from requests.adapters import HTTPAdapter
 from urllib3.poolmanager import PoolManager
+import getpass;
+from bs4 import BeautifulSoup
+
 
 class SSLAdapter(HTTPAdapter):
     def init_poolmanager(self, *args, **kwargs):
@@ -14,19 +17,29 @@ class SSLAdapter(HTTPAdapter):
         kwargs['ssl_context'] = context
         return super(SSLAdapter, self).init_poolmanager(*args, **kwargs)
 
-cprint("Benvinguts a l'script de tasques d'aules. Versió 1.3. (twitter: @josep_g)", 'green', 'on_grey')
+cprint("Nuevo y definitivo script de tareas de Aules", 'green', 'on_grey')
 
 session = requests.Session()
 session.mount('https://', SSLAdapter())
 session.cookies = requests.cookies.RequestsCookieJar()
 
-login_url = "https://aules.edu.gva.es/moodle/login/index.php"
+login_url = "https://aules.edu.gva.es/eso03/login/index.php"
 response = session.get(login_url)
+
+# Parse the HTML response
+soup = BeautifulSoup(response.text, 'html.parser')
+
+# Extract the 'logintoken' property
+logintoken = soup.find('input', {'name': 'logintoken'})['value']
+
+# Print the logintoken
+print(f'Login Token: {logintoken}')
+
 
 login_data = {
     'username': input("Introduce tu nombre de usuario: "),
-    'password': input("Introduce tu contraseña: "),
-    'loginbtn': 'Login'
+    'password': getpass.getpass("Introduce tu contraseña: "),
+    'logintoken': logintoken
 }
 
 response = session.post(login_url, data=login_data)
@@ -34,19 +47,56 @@ returnPage = response.text
 
 if 'loginerrormessage' in returnPage:
     cprint("Error iniciant la sessió en aules", 'red', 'on_white')
+    print(returnPage)
     sys.exit()
 
 totalTasks = 0
 
-pattern = re.compile(r'(https\:\/\/aules[0-9]?.edu.gva.es\/moodle\/course\/view.php\?id=[0-9]+)">(.*)</a>')
+# Parse the HTML response
+soup = BeautifulSoup(returnPage, 'html.parser')
+
+# Extract the 'sesskey' property
+sesskey = soup.find('input', {'name': 'sesskey'})['value']
+
+# Print the sesskey
+print(f'Sesskey: {sesskey}')
+
+
+# Se deben capturar los cursos de https://aules.edu.gva.es/eso03/lib/ajax/service.php?sesskey=theactualkey&info=core_course_get_enrolled_courses_by_timeline_classification
+
+courses_url = f"https://aules.edu.gva.es/eso03/lib/ajax/service.php?sesskey={sesskey}&info=core_course_get_enrolled_courses_by_timeline_classification"
+courses_data_payload = [
+    {
+        "index": 0,
+        "methodname": "core_course_get_enrolled_courses_by_timeline_classification",
+        "args": {
+            "offset": 0,
+            "limit": 0,
+            "classification": "all",
+            "sort": "fullname",
+            "customfieldname": "",
+            "customfieldvalue": ""
+        }
+    }
+]
+response = session.post(courses_url, json=courses_data_payload)
+courses_data = response.json()
+
+# Print the courses data
+print(courses_data)
+
+pattern = re.compile(r'(https\:\/\/aules.edu.gva.es\/eso03\/course\/view.php\?id=[0-9]+)">(.*)</a>')
+print(re.findall(pattern, returnPage))
+
 for courseURL, courseName in re.findall(pattern, returnPage):
     result = courseURL.split("id=")
+    print(result)
     if int(result[1]) not in config.cursosExclosos:
         cprint(courseName, 'grey', 'on_cyan')
-        course_page_url = courseURL.split('moodle')[0] + 'moodle/mod/assign/index.php?id=' + result[1]
+        course_page_url = courseURL.split('eso03')[0] + 'eso03/mod/assign/index.php?id=' + result[1]
         url = session.get(course_page_url)
         returnPage = url.text
-        pattern2 = re.compile(r'(https\:\/\/aules[0-9]?.edu.gva.es\/moodle\/mod\/assign\/view.php\?id=[0-9]+)">(.*)</a>')
+        pattern2 = re.compile(r'(https\:\/\/aules[0-9]?.edu.gva.es\/eso03\/mod\/assign\/view.php\?id=[0-9]+)">(.*)</a>')
         for tascaURL, tascaName in re.findall(pattern2, returnPage):
             url = session.get(tascaURL)
             returnPage3 = url.text
