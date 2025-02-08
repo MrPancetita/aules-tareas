eb7dcc59ad678e4d03bbce969fc50104e1d4b5b6
MrPancetita <contreras.engineer@outlook.com>
2025-02-08 15:09:38 +0100

    v1, recupera toda la info pero en bruto...

--- tareas.py
+++ tareas.py
@@ -11,7 +11,18 @@ class Course:
         self.id = id
         self.name = name
         self.url = url
+    
+    def __str__(self):
+        return f"\nCourse ID: {self.id}\nCourse Name: {self.name}\nCourse URL: {self.url}\n"
 
+class Task:
+    def __init__(self, id, name, url):
+        self.id = id
+        self.name = name
+        self.url = url
+    
+    def __str__(self):
+        return f"\nTask ID: {self.id}\nTask Name: {self.name}\nTask URL: {self.url}\n"
 
 class SSLAdapter(HTTPAdapter):
     def init_poolmanager(self, *args, **kwargs):
@@ -20,6 +31,34 @@ class SSLAdapter(HTTPAdapter):
         context.verify_mode = ssl.CERT_NONE
         kwargs['ssl_context'] = context
         return super(SSLAdapter, self).init_poolmanager(*args, **kwargs)
+    
+
+def get_tarea_grading(session, task):
+    task_id = task.id
+    grading_url = f"https://aules.edu.gva.es/eso03/mod/assign/view.php?id={task_id}&action=grading"
+    response = session.get(grading_url)
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+    rows = soup.find_all('tr', class_=lambda x: x and 'user' in x)
+
+    for fila in rows:
+        # Encuentra la fila específica por su id
+        # Extrae las columnas deseadas
+        c2 = fila.find('td', class_='c2').text.strip()
+        c3 = fila.find('td', class_='c3').text.strip()
+        c4 = fila.find('td', class_='c4').text.strip()
+        c7 = fila.find('td', class_='c7').text.strip()
+        c8 = fila.find('td', class_='c8').text.strip()
+
+        # Muestra los resultados
+        print(f'Nombre: {c2}')
+        print(f'Email: {c3}')
+        print(f'Estatus de la Entrega: {c4}')
+        print(f'Fecha y Hora: {c7}')
+        print(f'Archivos Subidos: {c8}')
+
+
+    
 
 cprint("Nuevo y definitivo script de tareas de Aules", 'green', 'on_grey')
 
@@ -88,14 +127,37 @@ courses_data = response.json()[0]['data']['courses']
 
 course_list = []
 
-for course in courses_data:
-    if course['visible'] == False:
+for entry in courses_data:
+    if entry['visible'] == False:
         continue
-    course_id = course['id']
-    course_name = course['fullname']
+    course_id = entry['id']
+    course_name = entry['fullname']
     course_url = f"https://aules.edu.gva.es/eso03/course/view.php?id={course_id}"
     course = Course(course_id, course_name, course_url)
     course_list.append(course)
-    print(f"Course ID: {course_id}, Course Name: {course_name}, Course URL: {course_url}")
+    print(course)
 
-# Hasta aquí recupero la lista de cursos... Ahora falta identificar las tareas de cada curso, saltar al grading y de ahí scrapeo del estado...
+for course in course_list:
+    response = session.get(course.url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+        
+    # Aquí puedes agregar el código para identificar las tareas de cada curso y hacer el scraping del estado
+    # Por ejemplo, podrías buscar elementos específicos en la página del curso
+    tasks = soup.find_all('div', class_='modtype_assign')
+    print(f'Course: {course.name}, Tasks: {len(tasks)}')
+    print('*' * 50)
+    print(f'Detalle de las tareas del curso {course.name}:')
+    for entry in tasks:
+        task_name = entry.find('span', class_='instancename').text.strip()
+        task_url = entry.find('a')['href']
+        task_id = task_url.split('id=')[-1] if 'id=' in task_url else 'N/A'
+        task = Task(task_id, task_name, task_url)
+        print(f'Task Name: {task_name}, Task URL: {task_url}, Task ID: {task_id}')
+        print(f'Recuperando entregas de la tarea...')
+        get_tarea_grading(session, task)  
+        print('*' * 50)
+    print('\n\n')
+
+
+
+
